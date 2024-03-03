34640fdc7268424f61d150533cc409630c9503f2
Chandraprakash <mamidipraveenreddy@Mamidis-MacBook-Air.local>
2024-03-03 11:57:25 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CloneGame from './Game/CloneGame';
-import Calculator from './Calculator/Calculator'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <Calculator></Calculator>
-      {/* <CloneGame></CloneGame> */}
+      <CloneGame></CloneGame>
   </React.StrictMode>
 );
 
